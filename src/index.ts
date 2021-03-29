da1e63bee30d821ae22775e157ca2e1207c5b9e3
Sun-2 <sun127.0.0.1@gmail.com>
2021-03-29 22:29:30 +0200

    init

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import { Client } from "discord.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+(async () => {
+  const client = new Client();
+  client.on("message", ev => {
+    console.log("msg", ev.content);
+    if (ev.content === "A można?") {
+      ev.channel.send("Jeszcze jak!");
+    }
+  });
+
+
+
+  client.login(process.env.TOKEN);
+})();
